cdd95918627975da0601fae46e983d97e83e9d51
apoorvkh <apoorv.khand@gmail.com>
2022-04-26 11:59:03 -0700

    Undo condabin symlink code-server

--- .conda/condabin_symlinks.sh
+++ .conda/condabin_symlinks.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
-for binary in git zsh pdftex gpg git-secret code-server
+for binary in git zsh pdftex gpg git-secret
 do
     if [[ -a ~/.local/miniconda3/bin/$binary ]]; then
         ln -sf ~/.local/miniconda3/bin/$binary ~/.local/miniconda3/condabin/$binary;
