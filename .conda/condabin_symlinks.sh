a1647c5450c5ed47ab8ae9e8440d7f33861b22ef
apoorvkh <apoorv.khand@gmail.com>
2022-05-11 06:40:09 +0000

    Adding tensorboard, gdown to conda

--- .conda/condabin_symlinks.sh
+++ .conda/condabin_symlinks.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
-for binary in git zsh pdftex gpg git-secret zip
+for binary in git zsh pdftex gpg git-secret zip gdown tensorboard
 do
     if [[ -a ~/.local/miniconda3/bin/$binary ]]; then
         ln -sf ~/.local/miniconda3/bin/$binary ~/.local/miniconda3/condabin/$binary;
