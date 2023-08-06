2dc65d29db83a7ea9f03d21e47819c15e7297551
Apoorv Khandelwal <mail@apoorvkh.com>
2023-08-06 23:44:56 +0000

    Updated install script

--- .local/install.sh
+++ .local/install.sh
@@ -4,19 +4,22 @@
 
 cd ~
 
-export BRANCH="${1:-main}"
-git init -b $BRANCH
+export DOTFILES_BRANCH="${1:-main}"
+git init -b $DOTFILES_BRANCH
 git remote add origin https://github.com/apoorvkh/dotfiles.git
 git fetch
-git checkout origin/$BRANCH -ft
+git checkout origin/$DOTFILES_BRANCH -ft
 git remote set-url origin git@github.com:apoorvkh/dotfiles.git
+unset DOTFILES_BRANCH
 
 # micromamba
 
 export MAMBA_ROOT_PREFIX="$HOME"
+export CONDA_PKGS_DIRS=$HOME/.cache/conda/pkgs
 eval "$(~/.local/bin/micromamba shell hook -s posix)"
-
 micromamba create -f ~/.environment.yml
+micromamba clean --all --quiet --yes
+micromamba activate _base
 
 # oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
+curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | ZSH=~/.oh-my-zsh KEEP_ZSHRC=yes CHSH=no RUNZSH=no bash -s --
